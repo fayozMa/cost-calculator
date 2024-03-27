78ec479cb434f197bdd7f70f2e3e13262266ecc0
Muhammadfayyozbek Ahmadjonov <usmonovbahriddin108@gmail.com>
2024-03-27 19:49:17 +0500

    complate

---
+++ index.js
@@ -0,0 +1,15 @@
+let money;
+money = prompt('Enter your total cash. In sum')
+let ticket = 500
+let hotel = 250
+let museum = 120
+let dollartosum = 11000.34
+let eurtosum = 12354.03
+let total = (500*dollartosum) + (250*dollartosum) + (120*eurtosum)
+if(money>=total){
+    console.log("Oq yo'l, Alisher!");
+}
+else{
+    console.log(" Alisher, ozgina sabr qilish kerak bo'lar ekan.");
+}
+
